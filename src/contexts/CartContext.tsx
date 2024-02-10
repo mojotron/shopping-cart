3a0c00e6d69a18aa8fe9628807b43e94879e904d
mojo <igormalj@gmail.com>
2024-02-10 20:23:19 +0100

    makeing design plans

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,6 +1,34 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useReducer } from 'react';
+import { ProductType } from '../types/productType';
 
-const useCartSource = () => {};
+type StatusType = 'loading' | 'ready' | 'shipment' | 'payment';
+
+type StateType = {
+  cart: ProductType[];
+  status: StatusType;
+};
+
+type ActionsType =
+  | { type: 'cart/add-item'; payload: ProductType }
+  | { type: 'cart/remove-item' };
+
+const reducer = (state: StateType, action: ActionsType): StateType => {
+  switch (action.type) {
+    case 'cart/add-item': {
+      return { ...state, cart: [...state.cart, action.payload] };
+    }
+    default: {
+      return { ...state };
+    }
+  }
+};
+
+const useCartSource = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    cart: [],
+    status: 'loading',
+  });
+};
 
 export const CartContext = createContext<ReturnType<typeof useCartSource>>(
   {} as unknown as ReturnType<typeof useCartSource>
