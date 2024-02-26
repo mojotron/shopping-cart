fe3ab10dde1b473e0a11991bb277b972445a0fc6
mojo <igormalj@gmail.com>
2024-02-26 21:07:27 +0100

    add input field ui component

--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BillingDetails from './components/BillingDetails';
 import OrderSummary from './components/OrderSummary';
+import InputField from '../../ui/InputField/InputField';
 
 const CheckoutPage = () => {
+  const [cardData, setCardData] = useState({ name: '' });
   return (
     <div>
       <BillingDetails />
       <OrderSummary />
+      <InputField
+        id="temp"
+        label="name"
+        placeholder="enter name"
+        value={cardData.name}
+        onChange={(value: string) =>
+          setCardData((oldValue) => ({ ...oldValue, name: value }))
+        }
+        type="text"
+      />
     </div>
   );
 };
--- src/ui/InputField/InputField.tsx
+++ src/ui/InputField/InputField.tsx
@@ -1,9 +1,39 @@
 type PropsType = {
-  type: string;
+  id: string;
+  label: string;
+  placeholder: string;
+  type?: 'text';
+  name?: string;
+  required?: boolean;
+  value: string;
+  onChange: (value: string) => void;
 };
 
-const InputField = () => {
-  return <div>InputField</div>;
+const InputField = ({
+  id,
+  label,
+  placeholder,
+  type = 'text',
+  required = false,
+  name = '',
+  value,
+  onChange,
+}: PropsType) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor={id}>{label}</label>
+      <input
+        className="border border-emerald-400 px-4 py-1 rounded-md text-emerald-600 font-bold placeholder:text-neutral-400 outline-none focus:ring focus:ring-emerald-200 focus:ring-offset-0"
+        id={id}
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        type={type}
+        name={name}
+      />
+    </div>
+  );
 };
 
 export default InputField;
